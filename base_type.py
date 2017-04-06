33b4e1ca47b7415721505657c682e69d72157ece
Danny Vilela <dov205@nyu.edu>
2017-04-06 03:12:57 -0400

    update base type documentation

--- base_type.py
+++ base_type.py
@@ -1,5 +1,16 @@
 
 def base_type_check(val, cast):
+    """All-in-one function to determine valid value castings.
+
+    This function takes a value :val and a casting function :cast
+    and attempts to cast :val as a value of type :cast. We do this
+    to determine potential base type values.
+
+    :param val: any value of any type
+    :param cast: cast function to be applied to :val
+    :return (Bool, val): tuple denoting whether the value was correctly
+        able to be cast to type :cast and the original value
+    """
     try:
         attempt = cast(val)
 
