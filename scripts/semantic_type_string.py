64a731b3702cf5008079f32b71a4bd383484158e
Charles Guthrie <charles.d.guthrie@gmail.com>
2017-04-16 20:14:22 -0400

    cleaned up semantic type string a bit

--- scripts/semantic_type_string.py
+++ scripts/semantic_type_string.py
@@ -35,7 +35,6 @@ school_region_semantic = re.compile(r'(Alternative|Region|District)')
 school_region_valid = re.compile(r'.+\s[1-9]$|.+\s10$|.+\s75$|.+\sSuperintendency$')
 school_region_args = {'semantic_match': school_region_semantic.match,
                       'valid_check': school_region_valid.match}
-                      
 is_school_region = partial(_semantic_validity_factory, semantic_name='school_region', **school_region_args)
 
 
@@ -43,7 +42,6 @@ is_school_region = partial(_semantic_validity_factory, semantic_name='school_reg
 school_num_semantic = re.compile(r'([BMQRX]|\b)\d{1,3}(\b|[A-Z]\d{0,2}|\-\d{2,3}(\b|[A-Z]))')
 school_num_args = {'semantic_match': school_num_semantic.match,
                    'valid_check': lambda x: len(x) <= 8}
-
 is_school_number = partial(_semantic_validity_factory, semantic_name='school_num', **school_num_args)
 
 
@@ -51,7 +49,6 @@ is_school_number = partial(_semantic_validity_factory, semantic_name='school_num
 address_name_semantic = re.compile(r'([\w\,\.\-]+ *)+')
 address_name_args = {'semantic_match': address_name_semantic.match,
                      'valid_check': lambda x: True}
-
 is_address_name = partial(_semantic_validity_factory, semantic_name='address_name', **address_name_args)
 
 
